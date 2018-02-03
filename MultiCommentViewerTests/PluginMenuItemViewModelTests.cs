ca4e37deca056c5a07e5a97c14a499d0469a9e12
Ryu Shinmura <rysestock@gmail.com>
2018-02-03 17:14:30 +0900

    add PluginMenuItemViewModelのテストを追加

---
+++ MultiCommentViewerTests/PluginMenuItemViewModelTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MultiCommentViewer;
+using NicoSitePlugin;
+using Plugin;
+using Moq;
+using NUnit.Framework;
+namespace MultiCommentViewerTests
+{
+    [TestFixture]
+    class PluginMenuItemViewModelTests
+    {
+        [Test]
+        public void プラグインメニューのコマンドが実行されると設定画面表示メソッドが呼ばれるか()
+        {
+            var mock = new Mock<IPlugin>();
+            var vm = new PluginMenuItemViewModel(mock.Object);
+            vm.ShowSettingViewCommand.Execute(null);
+            mock.Verify(m => m.ShowSettingView());
+        }
+    }
+}

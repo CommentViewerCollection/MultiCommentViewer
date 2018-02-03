af2f7d5964f86e69ec9251eb70fc4b7f531f1a90
Ryu Shinmura <rysestock@gmail.com>
2018-02-03 17:50:49 +0900

    update 問題の切り分けのために導入したclassを削除

--- MultiCommentViewer/MainViewModel.cs
+++ MultiCommentViewer/MainViewModel.cs
@@ -554,7 +554,7 @@ namespace MultiCommentViewer
         public PluginMenuItemViewModel(IPlugin plugin)// PluginContext plugin, string name, ICommand command)
         {
             Name = plugin.Name;            
-            ShowSettingViewCommand = new Ttk.RelayCommand(() => Test(plugin));
+            ShowSettingViewCommand = new RelayCommand(() => Test(plugin));
         }
         private void Test(IPlugin plugin)
         {
@@ -570,102 +570,3 @@ namespace MultiCommentViewer
     }
 }
 
-
-
-
-namespace Ttk
-{
-    using GalaSoft.MvvmLight.Helpers;
-    using System;
-    using System.Runtime.CompilerServices;
-    using System.Windows.Input;
-    /// <summary>
-    /// A command whose sole purpose is to relay its functionality to other
-    /// objects by invoking delegates. The default return value for the CanExecute
-    /// method is 'true'.  This class does not allow you to accept command parameters in the
-    /// Execute and CanExecute callback methods.
-    /// </summary>
-    /// <remarks>If you are using this class in WPF4.5 or above, you need to use the 
-    /// GalaSoft.MvvmLight.CommandWpf namespace (instead of GalaSoft.MvvmLight.Command).
-    /// This will enable (or restore) the CommandManager class which handles
-    /// automatic enabling/disabling of controls based on the CanExecute delegate.</remarks>
-    public class RelayCommand : ICommand
-    {
-        private readonly WeakAction _execute;
-
-        private readonly WeakFunc<bool> _canExecute;
-
-        /// <summary>
-        /// Occurs when changes occur that affect whether the command should execute.
-        /// </summary>
-        [method: CompilerGenerated]
-        [CompilerGenerated]
-        public event EventHandler CanExecuteChanged;
-
-        /// <summary>
-        /// Initializes a new instance of the RelayCommand class that 
-        /// can always execute.
-        /// </summary>
-        /// <param name="execute">The execution logic. IMPORTANT: Note that closures are not supported at the moment
-        /// due to the use of WeakActions (see http://stackoverflow.com/questions/25730530/). </param>
-        /// <exception cref="T:System.ArgumentNullException">If the execute argument is null.</exception>
-        public RelayCommand(Action execute) : this(execute, null)
-        {
-        }
-
-        /// <summary>
-        /// Initializes a new instance of the RelayCommand class.
-        /// </summary>
-        /// <param name="execute">The execution logic. IMPORTANT: Note that closures are not supported at the moment
-        /// due to the use of WeakActions (see http://stackoverflow.com/questions/25730530/). </param>
-        /// <param name="canExecute">The execution status logic.</param>
-        /// <exception cref="T:System.ArgumentNullException">If the execute argument is null. IMPORTANT: Note that closures are not supported at the moment
-        /// due to the use of WeakActions (see http://stackoverflow.com/questions/25730530/). </exception>
-        public RelayCommand(Action execute, Func<bool> canExecute)
-        {
-            if (execute == null)
-            {
-                throw new ArgumentNullException("execute");
-            }
-            this._execute = new WeakAction(execute);
-            if (canExecute != null)
-            {
-                this._canExecute = new WeakFunc<bool>(canExecute);
-            }
-        }
-
-        /// <summary>
-        /// Raises the <see cref="E:GalaSoft.MvvmLight.Command.RelayCommand.CanExecuteChanged" /> event.
-        /// </summary>
-        public void RaiseCanExecuteChanged()
-        {
-            EventHandler canExecuteChanged = this.CanExecuteChanged;
-            if (canExecuteChanged != null)
-            {
-                canExecuteChanged(this, EventArgs.Empty);
-            }
-        }
-
-        /// <summary>
-        /// Defines the method that determines whether the command can execute in its current state.
-        /// </summary>
-        /// <param name="parameter">This parameter will always be ignored.</param>
-        /// <returns>true if this command can be executed; otherwise, false.</returns>
-        public bool CanExecute(object parameter)
-        {
-            return this._canExecute == null || ((this._canExecute.IsStatic || this._canExecute.IsAlive) && this._canExecute.Execute());
-        }
-
-        /// <summary>
-        /// Defines the method to be called when the command is invoked. 
-        /// </summary>
-        /// <param name="parameter">This parameter will always be ignored.</param>
-        public virtual void Execute(object parameter)
-        {
-            if (this.CanExecute(parameter) && this._execute != null && (this._execute.IsStatic || this._execute.IsAlive))
-            {
-                this._execute.Execute();
-            }
-        }
-    }
-}

b9fa2535796c296bbfa19093a5547e96f1c9bb8e
Ryu Shinmura <rysestock@gmail.com>
2018-02-13 00:28:18 +0900

    update OptionsをDynamicOptionsBaseを継承したものに書き換えた

--- BouyomiPlugin/Options.cs
+++ BouyomiPlugin/Options.cs
@@ -1,46 +1,24 @@
 ﻿using System;
 using System.ComponentModel;
+using Common;
 namespace BouyomiPlugin
 {
-    class Options : INotifyPropertyChanged
+    class Options : DynamicOptionsBase
     {
-        public bool IsEnabled { get; set; }
-        private string _BouyomiChanPath;
-        public string BouyomiChanPath
+        public bool IsEnabled { get { return GetValue(); } set { SetValue(value); } }
+        public string BouyomiChanPath { get { return GetValue(); } set { SetValue(value); } }
+        public bool IsReadHandleName { get { return GetValue(); } set { SetValue(value); } }
+        public bool IsReadComment { get { return GetValue(); } set { SetValue(value); } }
+        public bool IsAppendNickTitle { get { return GetValue(); } set { SetValue(value); } }
+        public string NickTitle { get { return GetValue(); } set { SetValue(value); } }
+        protected override void Init()
         {
-            get { return _BouyomiChanPath; }
-            set
-            {
-                if (_BouyomiChanPath == value)
-                    return;
-                _BouyomiChanPath = value;
-                RaisePropertyChanged();
-            }
+            Dict.Add(nameof(IsEnabled), new Item { DefaultValue = false, Predicate = b => true, Serializer = b => b.ToString(), Deserializer = s => bool.Parse(s) });
+            Dict.Add(nameof(BouyomiChanPath), new Item { DefaultValue = "", Predicate = s => true, Serializer = s => s, Deserializer = s => s });
+            Dict.Add(nameof(IsReadHandleName), new Item { DefaultValue = false, Predicate = b => true, Serializer = b => b.ToString(), Deserializer = s => bool.Parse(s) });
+            Dict.Add(nameof(IsReadComment), new Item { DefaultValue = false, Predicate = b => true, Serializer = b => b.ToString(), Deserializer = s => bool.Parse(s) });
+            Dict.Add(nameof(IsAppendNickTitle), new Item { DefaultValue = true, Predicate = b => true, Serializer = b => b.ToString(), Deserializer = s => bool.Parse(s) });
+            Dict.Add(nameof(NickTitle), new Item { DefaultValue = "さん", Predicate = s => true, Serializer = s => s, Deserializer = s => s });
         }
-        public bool IsReadHandleName { get; set; }
-        public bool IsReadComment { get; set; }
-        public bool IsAppendNickTitle { get; set; }
-        public string NickTitle { get; set; } = "さん";
-
-        #region INotifyPropertyChanged
-        [NonSerialized]
-        private System.ComponentModel.PropertyChangedEventHandler _propertyChanged;
-        /// <summary>
-        /// 
-        /// </summary>
-        public event System.ComponentModel.PropertyChangedEventHandler PropertyChanged
-        {
-            add { _propertyChanged += value; }
-            remove { _propertyChanged -= value; }
-        }
-        /// <summary>
-        /// 
-        /// </summary>
-        /// <param name="propertyName"></param>
-        protected void RaisePropertyChanged([System.Runtime.CompilerServices.CallerMemberName] string propertyName = "")
-        {
-            _propertyChanged?.Invoke(this, new System.ComponentModel.PropertyChangedEventArgs(propertyName));
-        }
-        #endregion
     }
 }

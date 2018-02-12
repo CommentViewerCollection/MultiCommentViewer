2e0be7ca9b9b2b293b471e95e8c3c9875c053ebd
Ryu Shinmura <rysestock@gmail.com>
2018-02-13 04:27:48 +0900

    update スペースが入ったパスにも対応
    Visual Studioのバグなのか何なのか"$(test) "のようにスペースを入れないとそれ以降も同じ引数だと認識されてしまうようで苦労した。。。
    https://stackoverflow.com/questions/6755654/build-events-not-honoring-multiple-command-line-arguments/6910500

--- MultiCommentViewer/postBuild.ps1
+++ MultiCommentViewer/postBuild.ps1
@@ -1,19 +1,19 @@
 param([String]$targetDir, [String]$targetName)
 $dllDirName = "dll"
 $pluginDirName = "plugins"
-$dllDir = $targetDir + "\" + $dllDirName
+$dllDir = "$targetDir" + "\" + $dllDirName
 
-Remove-Item -Recurse -Force "$dllDir" -ErrorAction Ignore
+Remove-Item -Recurse -Force $dllDir -ErrorAction Ignore
 New-Item "$dllDir" -type directory
 
-$Files = Get-ChildItem -Path "$targetDir"  -Exclude "$targetName.*" | where {! $_.PSIsContainer}
+$Files = Get-ChildItem -Path $targetDir  -Exclude "$targetName.*" | where {! $_.PSIsContainer}
 ForEach ($file in $Files)
 {
     Move-Item -Path "$File" -Destination "$dllDir"
 }
 
-$dirs = Get-ChildItem -Path "$targetDir"  -Exclude "$dllDirName","$pluginDirName" | where { $_.PSIsContainer }
+$dirs = Get-ChildItem -Path $targetDir  -Exclude $dllDirName,$pluginDirName | where { $_.PSIsContainer }
 ForEach ($dir in $dirs)
 {
-    Move-Item -Path "$dir" -Destination "$dllDir"
+    Move-Item -Path "$dir" -Destination $dllDir
 }
--- MultiCommentViewer/updateFileList.ps1
+++ MultiCommentViewer/updateFileList.ps1
@@ -1,6 +1,6 @@
 param([String]$targetDir)
 
-$filename = [System.String]::Format("{0}\\list.txt", $targetDir)
+$filename = [System.String]::Format("{0}\list.txt", "$targetDir")
 $Files = Get-ChildItem "$targetDir" -Name -Exclude "settings", "error.txt", "*.vshost.*" -Recurse -File
 
 Set-Content "$filename" $Files

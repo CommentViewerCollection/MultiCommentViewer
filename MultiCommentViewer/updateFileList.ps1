a4801bb15e31fec4909f93ba7a104d29a7fcd66e
Ryu Shinmura <rysestock@gmail.com>
2018-02-13 02:17:18 +0900

    fix Jenkinsの自動テストに失敗したためさらに修正

--- MultiCommentViewer/updateFileList.ps1
+++ MultiCommentViewer/updateFileList.ps1
@@ -1,7 +1,7 @@
 param([String]$targetDir)
 
 $filename = [System.String]::Format("{0}\\list.txt", $targetDir)
-$Files = Get-ChildItem $targetDir -Name -Exclude "settings", "error.txt", "*.vshost.*" -Recurse -File
+$Files = Get-ChildItem "$targetDir" -Name -Exclude "settings", "error.txt", "*.vshost.*" -Recurse -File
 
-Set-Content $filename $Files
-Add-Content $filename "list.txt"
+Set-Content "$filename" $Files
+Add-Content "$filename" "list.txt"

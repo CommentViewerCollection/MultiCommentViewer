0fecf05c167bf4e5f3fa3af05cfa6f61097cabb1
Ryu Shinmura <rysestock@gmail.com>
2018-03-11 02:11:52 +0900

    IEのCookieが取得できなくなっていたため修正

--- BrowserCookieImplementations/IE.cs
+++ BrowserCookieImplementations/IE.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Net;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
-
+using BrowserCookieImplementations.IE;
 namespace ryu_s.BrowserCookie
 {
     public class IEManager : IIEManager
@@ -70,127 +72,24 @@ namespace ryu_s.BrowserCookie
             /// <summary>
             /// 
             /// </summary>
-            /// <param name="high"></param>
-            /// <param name="low"></param>
-            /// <returns></returns>
-            public DateTime FromFileTime(long high, long low)
-            {
-                return new DateTime((high << 32) + low).AddYears(1600);
-            }
-            /// <summary>
-            /// 
-            /// </summary>
-            private class IECookieRaw
-            {
-                public string name;
-                public string value;
-                public string path;
-                public string domain;
-                public string Flags;
-                public string ExpirationTimeLow;
-                public string ExpirationTimeHigh;
-                public string CreationTimeLow;
-                public string CreationTimeHigh;
-            }
-            /// <summary>
-            /// 
-            /// </summary>
             /// <param name="domain"></param>
             /// <returns></returns>
             private IEnumerable<Cookie> GetCookieCollectionInternal(string domain)
             {
-                /*
-                 * Name
-                 * Value
-                 * Path
-                 * Flags (optional)
-                 * Expiration Time (low)
-                 * Expiration Time (high)
-                 * Creation Time (low)
-                 * Creation Time (high)
-                 * Delimiter (used to separate multiple cookie entries in a single file)
-                 * The expiration and creation times are in FILETIME format. 
-                 * You can find information on the FILETIME format here. Most importantly:
-                 * The FILETIME structure represents the number of 100-nanosecond intervals since January 1, 1601.
-                 * The structure consists of two 32-bit values that combine to form a single 64-bit value.
-                 * 
-                 * '*'がデリミタとして使われるが、値の削除の際にも使われるためそれだけでは判断材料としては足りない。
-                 * 1つのクッキーは7～8行と決まっており、Creation Time (high)が'*'になっている場所は確認できなかったため、7～8行目にあった場合はそれをデリミタであると断定して良さそう。
-                 * 
-                 */
-
-                var filePaths = new List<string>();
-                //ディレクトリは2種類あって、LowはLow privilegeの場合？
-                var filePaths1 = System.IO.Directory.GetFiles(Path, "*.txt", System.IO.SearchOption.TopDirectoryOnly);
-                filePaths.AddRange(filePaths1);
-                var filePaths2 = System.IO.Directory.GetFiles(Path, "*.cookie", System.IO.SearchOption.TopDirectoryOnly);
-                filePaths.AddRange(filePaths2);
-
-                var filePaths3 = System.IO.Directory.GetFiles(Path + @"\Low", "*.txt", System.IO.SearchOption.TopDirectoryOnly);
-                filePaths.AddRange(filePaths3);
-                var filePaths4 = System.IO.Directory.GetFiles(Path + @"\Low", "*.cookie", System.IO.SearchOption.TopDirectoryOnly);
-                filePaths.AddRange(filePaths4);
-
-                foreach (string path in filePaths)
+                var path = "/";
+                var cookieData = BrowserCookieImplementations.IE.Tools.GetCookieData("https://" + domain);
+                var cookies = CookieData2Cookies(cookieData, domain, path);
+                var cookieDataProtected = BrowserCookieImplementations.IE.Tools.GetProtectedModeCookieData("https://" + domain);
+                var cookiesProtected = CookieData2Cookies(cookieDataProtected, domain, path);
+                return cookies.Concat(cookiesProtected);
+            }
+            private IEnumerable<Cookie> CookieData2Cookies(string cookieData, string domain, string path)
+            {
+                var dict = BrowserCookieImplementations.IE.Tools.GetCookieDict(cookieData);
+                foreach (var kv in dict)
                 {
-                    List<string> fileLines = new List<string>();
-                    using (var sr = new System.IO.StreamReader(path))
-                    {
-                        while (!sr.EndOfStream)
-                        {
-                            fileLines.Add(sr.ReadLine());
-                        }
-                    }
-                    var contains = fileLines.Where(s => s.Contains(domain));
-                    if (contains.Count() <= 0)
-                        continue;//目的のCookieに該当する見込みなし
-
-                    int fileLineNum = 0;
-                    while (fileLineNum < fileLines.Count)
-                    {
-                        var lineNum = 0;
-                        var lineList = new List<string>();
-                        while (fileLineNum < fileLines.Count)
-                        {
-                            var line = fileLines[fileLineNum++];
-                            lineNum++;
-                            if (line == "*" && lineNum >= 7)//7～8行目の'*'はデリミタであると判断。
-                                break;
-                            lineList.Add(line);
-                        }
-                        var ieCookie = new IECookieRaw();
-                        int n = 0;
-                        ieCookie.name = lineList[n++];
-                        ieCookie.value = lineList[n++];
-                        var domainAndPath = lineList[n++];
-                        var arr = domainAndPath.Split('/');
-                        if (arr.Length >= 2)//一応チェックしているけど、確実にarr.Length==2であると考えて良いと思う。
-                        {
-                            ieCookie.domain = arr[0];
-                            ieCookie.path = "/" + arr[1];
-                        }
-                        if (lineList.Count >= 8)//行数が8行ある場合はオプションのFlagsがあるということ。
-                            ieCookie.Flags = lineList[n++];
-                        ieCookie.ExpirationTimeLow = lineList[n++];
-                        ieCookie.ExpirationTimeHigh = lineList[n++];
-                        ieCookie.CreationTimeLow = lineList[n++];
-                        ieCookie.CreationTimeHigh = lineList[n++];
-
-                        if (ieCookie.domain != null && ieCookie.domain.EndsWith(domain))
-                        {
-                            //https://msdn.microsoft.com/en-us/library/system.net.cookie.value(v=vs.110).aspx
-                            //The Value of a Cookie must not be null. The following characters are reserved and cannot be used for this property: semicolon, comma.
-                            if (ieCookie.value.Contains(';') || ieCookie.value.Contains(','))
-                                continue;
-                            var cookie = new Cookie(ieCookie.name, ieCookie.value, ieCookie.path, ieCookie.domain)
-                            {
-                                //UTCになってるっぽいから+9時間
-                                Expires = FromFileTime(long.Parse(ieCookie.ExpirationTimeHigh), long.Parse(ieCookie.ExpirationTimeLow)).AddHours(9)
-                            };
-                            cookie.Expired = (cookie.Expires < DateTime.Now);
-                            yield return cookie;
-                        }
-                    }
+                    var cookie = new Cookie(kv.Key, kv.Value) { Domain = domain, Path = path };
+                    yield return cookie;
                 }
             }
         }
---
+++ BrowserCookieImplementations/IE/NativeMethods.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BrowserCookieImplementations.IE
+{
+    [Serializable]
+    public class IECookieRetrieveFailedException : Exception
+    {
+        public string Detail { get; }
+        public IECookieRetrieveFailedException(string message, string detail) : base(message)
+        {
+            Detail = detail;
+        }
+    }
+    static class NativeMethods
+    {
+        public const int S_OK = 0;
+        public const int ERROR_INVALID_PARAMETER = 87;
+        public const int ERROR_INSUFFICIENT_BUFFER = 122;
+        public const int ERROR_NO_MORE_ITEMS = 259;
+        public const int ERROR_INTERNET_UNRECOGNIZED_SCHEME = 12006;
+
+        public const int INTERNET_COOKIE_HTTPONLY = 0x2000;
+        [DllImport("wininet.dll", CharSet = CharSet.Unicode, SetLastError = true)]
+        public static extern bool InternetGetCookieEx(string pchURL, string pchCookieName, StringBuilder pchCookieData, ref uint pcchCookieData, int dwFlags, IntPtr lpReserved);
+        [DllImport("Ieframe.dll", CharSet = CharSet.Unicode, SetLastError = true)]
+        public extern static long IEIsProtectedModeProcess(ref bool pbResult);
+        [DllImport("Ieframe.dll", CharSet = CharSet.Unicode, SetLastError = true)]
+        public extern static long IEGetProtectedModeCookie(string lpszURL, string lpszCookieName, StringBuilder pszCookieData, ref uint pcchCookieData, uint dwFlags);
+
+    }
+}

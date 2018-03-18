ea30639e6798fd4d929e0f86f952a400894d5b21
Ryu Shinmura <rysestock@gmail.com>
2018-03-18 12:27:51 +0900

    InfoTypeを追加

--- Common/InfoCommentViewModel.cs
+++ Common/InfoCommentViewModel.cs
@@ -1,20 +1,60 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using SitePlugin;
 namespace Common
 {
-    public class InfoCommentViewModel : CommentViewModelBase
+    public class InfoCommentViewModel : CommentViewModelBase, IInfoCommentViewModel
     {
         public override string UserId => "-";
-        public InfoCommentViewModel(ICommentOptions options, string message)
-            :base(options)
+
+        public InfoType Type { get; }
+
+        public InfoCommentViewModel(ICommentOptions options, string message, InfoType type)
+            : base(options)
         {
             IsInfo = true;
             MessageItems = new List<IMessagePart>
             {
                 new MessageText(message),
             };
+            Type = type;
+        }
+        [Obsolete]
+        public InfoCommentViewModel(ICommentOptions options, string message)
+            : base(options)
+        {
+            IsInfo = true;
+            MessageItems = new List<IMessagePart>
+            {
+                new MessageText(message),
+            };
+            Type =  InfoType.Debug;
         }
     }
+    public interface IInfoCommentViewModel : ICommentViewModel
+    {
+        InfoType Type { get; }
+    }
+    public enum InfoType
+    {
+        /// <summary>
+        /// 無し
+        /// </summary>
+        None,
+        /// <summary>
+        /// 例外全て
+        /// </summary>
+        Debug,
+        /// <summary>
+        /// 
+        /// </summary>
+        Notice,
+        /// <summary>
+        /// 致命的なエラーがあった場合だけ。必要最小限の情報
+        /// </summary>
+        Error,
+    }
+
 }

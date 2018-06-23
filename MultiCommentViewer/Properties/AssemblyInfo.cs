f85c2f3e75abbb4199b00aa4cdeb7e23846d6520
Ryu Shinmura <rysestock@gmail.com>
2018-06-24 02:09:04 +0900

    バージョン番号をインクリメント

--- MultiCommentViewer/Properties/AssemblyInfo.cs
+++ MultiCommentViewer/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Windows;
 [assembly: AssemblyTitle("MultiCommentViewer")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("ryu-s")]
 [assembly: AssemblyProduct("MultiCommentViewer")]
 [assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
@@ -51,7 +51,7 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.1.14")]
-[assembly: AssemblyFileVersion("0.1.14.0")]
+[assembly: AssemblyVersion("0.1.15")]
+[assembly: AssemblyFileVersion("0.1.15.0")]
 
 [assembly:InternalsVisibleTo("MultiCommentViewerTests")]

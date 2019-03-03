00d0fe60d4312ab4ebcca8fab21c948c9a57de7b
Ryu Shinmura <rysestock@gmail.com>
2019-03-03 17:32:35 +0900

    IMetadataのデフォルト実装をSitePluginCommonに作成

---
+++ SitePluginCommon/Metadata.cs
@@ -0,0 +1,14 @@
+﻿using SitePlugin;
+
+namespace SitePluginCommon
+{
+    public class Metadata : IMetadata
+    {
+        public string Title { get; set; }
+        public string Elapsed { get; set; }
+        public string CurrentViewers { get; set; }
+        public string Active { get; set; }
+        public string TotalViewers { get; set; }
+        public bool? IsLive { get; set; }
+    }
+}

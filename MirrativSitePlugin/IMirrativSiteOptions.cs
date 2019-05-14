cafadb92acec5ac4fd31cc23a85aea88c7c585b4
Ryu Shinmura <rysestock@gmail.com>
2019-05-15 00:19:48 +0900

    Mirrativの入室メッセージを取得するしないを選べるようにした

--- MirrativSitePlugin/IMirrativSiteOptions.cs
+++ MirrativSitePlugin/IMirrativSiteOptions.cs
@@ -6,6 +6,8 @@ namespace MirrativSitePlugin
     interface IMirrativSiteOptions:INotifyPropertyChanged
     {
         bool NeedAutoSubNickname { get; }
+        bool IsShowJoinMessage { get; }
+        bool IsShowLeaveMessage { get; }
         Color ItemForeColor { get; }
         Color ItemBackColor { get; }
         int PollingIntervalSec { get; }

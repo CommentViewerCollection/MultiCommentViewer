96b00eae2eb599bf93bb538de1b9cd8bdbc8d7b0
Ryu Shinmura <rysestock@gmail.com>
2019-06-23 01:15:06 +0900

    ウインドウの色を変更できる機能を削除

--- MultiCommentViewer/Views/MainWindow.xaml.cs
+++ MultiCommentViewer/Views/MainWindow.xaml.cs
@@ -19,7 +19,7 @@ namespace MultiCommentViewer
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : CustomWindow.CustomChrome
+    public partial class MainWindow : Window
     {
         public MainWindow()
         {

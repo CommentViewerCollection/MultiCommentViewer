8611675f1d54bf0d32e23cd27202996c7becf14f
Ryu Shinmura <rysestock@gmail.com>
2019-07-17 02:39:06 +0900

    バージョン番号をインクリメント

--- MultiCommentViewer/Properties/AssemblyInfo.cs
+++ MultiCommentViewer/Properties/AssemblyInfo.cs
@@ -51,6 +51,6 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.4.1")]
+[assembly: AssemblyVersion("0.4.2")]
 
 [assembly:InternalsVisibleTo("MultiCommentViewerTests")]

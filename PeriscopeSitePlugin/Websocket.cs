5d62cbd8e27c3e83decc7589e02dde8e92e4c5ac
Ryu Shinmura <rysestock@gmail.com>
2019-08-10 15:56:54 +0900

    Periscopeのwebsocketが時々切れる問題に対応したつもり

--- PeriscopeSitePlugin/Websocket.cs
+++ PeriscopeSitePlugin/Websocket.cs
@@ -24,8 +24,8 @@ namespace PeriscopeSitePlugin
             var userAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.3359.139 Safari/537.36";
             _ws = new WebSocket(url, "", cookies, null, userAgent)
             {
-                EnableAutoSendPing = false,
-                AutoSendPingInterval = 0,
+                EnableAutoSendPing = true,
+                AutoSendPingInterval = 10*1000,
                 ReceiveBufferSize = 8192,
                 NoDelay = true
             };

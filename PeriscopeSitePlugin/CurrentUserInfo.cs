ba156186d786ff7097b89b331a96db7655810d0b
Ryu Shinmura <rysestock@gmail.com>
2019-12-11 02:22:20 +0900

    PeriscopeでチャンネルURLでコメント取得できるようにした

---
+++ PeriscopeSitePlugin/CurrentUserInfo.cs
@@ -0,0 +1,22 @@
+﻿using Common;
+using ryu_s.BrowserCookie;
+using SitePlugin;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Linq;
+using System.Threading;
+using SitePluginCommon;
+using SitePluginCommon.AutoReconnector;
+
+namespace PeriscopeSitePlugin
+{
+    class CurrentUserInfo : ICurrentUserInfo
+    {
+        public string Username { get; set; }
+        public string UserId { get; set; }
+        public bool IsLoggedIn { get; set; }
+    }
+}
--- PeriscopeSitePluginTests/ToolsTests.cs
+++ PeriscopeSitePluginTests/ToolsTests.cs
@@ -1,4 +1,5 @@
-﻿using NUnit.Framework;
+﻿using Codeplex.Data;
+using NUnit.Framework;
 using PeriscopeSitePlugin;
 using System;
 using System.Collections.Generic;
@@ -16,6 +17,7 @@ namespace PeriscopeSitePluginTests
         {
             Assert.AreEqual(("fave-musician", "1ypJdvRwXQVKW"), Tools.ExtractChannelNameAndLiveId("https://www.periscope.tv/fave-musician/1ypJdvRwXQVKW"));
             Assert.AreEqual(("fave-musician", "1ypJdvRwXQVKW"), Tools.ExtractChannelNameAndLiveId("https://www.pscp.tv/fave-musician/1ypJdvRwXQVKW"));
+            Assert.AreEqual(("fave-musician", (string)null), Tools.ExtractChannelNameAndLiveId("https://www.pscp.tv/fave-musician"));
             Assert.AreEqual(("fave-musician", "1ypJdvRwXQVKW"), Tools.ExtractChannelNameAndLiveId("https://www.periscope.tv/w/1ypJdvRwXQVKW?channel=fave-musician"));
             Assert.AreEqual(("fave-musician", "1ypJdvRwXQVKW"), Tools.ExtractChannelNameAndLiveId("https://www.pscp.tv/w/1ypJdvRwXQVKW?channel=fave-musician"));
             Assert.AreEqual(((string)null, "1ypJdvRwXQVKW"), Tools.ExtractChannelNameAndLiveId("https://www.periscope.tv/w/1ypJdvRwXQVKW"));
@@ -26,6 +28,32 @@ namespace PeriscopeSitePluginTests
         public void ExtractHostnameFromEndpointTest()
         {
             Assert.AreEqual("prod-chatman-ancillary-us-east-1.pscp.tv", Tools.ExtractHostnameFromEndpoint("https://prod-chatman-ancillary-us-east-1.pscp.tv"));
+        }
+        [Test]
+        public void UrlTest()
+        {
+            var livePageUrl = Tools.GetUrl("https://www.pscp.tv/JLSwitzerland/1vAxRqWmZLXJl") as LivePageUrl;
+            Assert.IsNotNull(livePageUrl);
+            Assert.AreEqual("https://www.pscp.tv/JLSwitzerland/1vAxRqWmZLXJl", livePageUrl.Url);
+            Assert.AreEqual("1vAxRqWmZLXJl", livePageUrl.LiveId);
+            Assert.AreEqual("JLSwitzerland", livePageUrl.ChannelId);
+
+            var channelUrl = Tools.GetUrl("https://www.pscp.tv/JLSwitzerland") as ChannelUrl;
+            Assert.IsNotNull(channelUrl);
+            Assert.AreEqual("https://www.pscp.tv/JLSwitzerland", channelUrl.Url);
+            Assert.AreEqual("JLSwitzerland", channelUrl.ChannelId);
+
+            var unknownUrl = Tools.GetUrl("abc") as UnknownUrl;
+            Assert.IsNotNull(unknownUrl);
+            Assert.AreEqual("abc", unknownUrl.Url);
+        }
+        [Test]
+        public void ExtractChannelPageJsonTest()
+        {
+            var data = TestHelper.GetSampleData("ChannelPageHtml_Running.txt");
+            var json = Tools.ExtractChannelPageJson(data);
+            var d = DynamicJson.Parse(json);
+            Assert.IsTrue(d.IsDefined("BroadcastCache"));
         }
     }
 }

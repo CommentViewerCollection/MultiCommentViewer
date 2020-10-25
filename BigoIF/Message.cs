93bb7a55424f39414ada31e949ebb81a48eeb367
Ryu Shinmura <rysestock@gmail.com>
2020-10-25 15:55:54 +0900

    BigoIFがgitに追加できていなかったため対応

---
+++ BigoIF/Message.cs
@@ -0,0 +1,37 @@
+﻿using SitePlugin;
+using System;
+using System.Collections.Generic;
+
+namespace BigoSitePlugin
+{
+    public enum BigoMessageType
+    {
+        Unknown,
+        Comment,
+        Superchat,
+        Connected,
+        Disconnected,
+    }
+
+
+    public interface IBigoMessage : ISiteMessage
+    {
+        BigoMessageType BigoMessageType { get; }
+    }
+    public interface IBigoConnected : IBigoMessage
+    {
+        string Text { get; }
+    }
+    public interface IBigoDisconnected : IBigoMessage
+    {
+        string Text { get; }
+    }
+    public interface IBigoComment : IBigoMessage
+    {
+        string Name { get; }
+        string Message { get; }
+        DateTime PostedAt { get; }
+        //string Id { get; }
+        //string UserId { get; }
+    }
+}

dfd0251a4990471bad2427e53e32973d72d4a527
utyosu <utyosu@gmail.com>
2021-09-19 19:27:49 +0900

    Propertiesの削除

--- MixchSitePlugin/Properties/AssemblyInfo.cs
+++
@@ -1,7 +0,0 @@
-﻿using System.Reflection;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
-
-[assembly: InternalsVisibleTo("MixchSitePluginTests")]
-[assembly: InternalsVisibleTo("DynamicProxyGenAssembly2")]
-[assembly: InternalsVisibleTo("SitePluginTests")]

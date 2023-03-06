1c6e8e7ab395c298de53a9033664e854fc19750a
Ryu Shinmura <rysestock@gmail.com>
2023-03-06 22:36:58 +0900

    一部の配信サイトで一定時間内に一定回数以上の自動再接続を行わないようにした

--- SitePluginCommon/AutoReconnection/ConnectionManager.cs
+++ SitePluginCommon/AutoReconnection/ConnectionManager.cs
@@ -6,6 +6,37 @@ using System.Threading.Tasks;
 
 namespace SitePluginCommon.AutoReconnection
 {
+    public class ReconnectionCounter
+    {
+        Queue<DateTime> _list = new Queue<DateTime>();
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns>一定時間内のAdd回数が一定回数以下だったらtrue</returns>
+        public bool Add(DateTime now)
+        {
+            const int n = 5;
+            const int range = 60 * 1000;
+            _list.Enqueue(now);
+            if (_list.Count <= n)
+            {
+                return true;
+            }
+            //直近のn個より前のデータを全て消す
+            for (int i = _list.Count - n; i > 0; i--)
+            {
+                _list.Dequeue();
+            }
+            //今あるn個のデータが全て基準時刻移行のものだったらfalseを返す
+            var baseTime = now.AddMilliseconds(-range);
+            var b = _list.All(h => h > baseTime);
+            return !b;
+        }
+        public void Reset()
+        {
+            _list.Clear();
+        }
+    }
     /// <summary>
     /// 複数のIProviderを管理
     /// </summary>
@@ -40,8 +71,10 @@ namespace SitePluginCommon.AutoReconnection
             {
                 throw new ArgumentException();
             }
+            _reconnectionCounterDict.Clear();
             foreach (var provider in group)
             {
+                _reconnectionCounterDict.Add(provider, new ReconnectionCounter());
                 provider.Start();
             }
             var workingProviders = new List<IProvider>(group);
@@ -65,8 +98,11 @@ namespace SitePluginCommon.AutoReconnection
                     if (!provider.Master.IsFinished)
                     {
                         //エラーによる終了が一定時間に一定回数発生したら継続しないようにしたい。
-                        provider.Start();
-                        workingProviders.Add(provider);
+                        if (_reconnectionCounterDict[provider].Add(DateTime.Now))
+                        {
+                            provider.Start();
+                            workingProviders.Add(provider);
+                        }
                     }
                 }
             }
@@ -98,5 +134,6 @@ namespace SitePluginCommon.AutoReconnection
         {
             _logger = logger;
         }
+        Dictionary<IProvider, ReconnectionCounter> _reconnectionCounterDict = new Dictionary<IProvider, ReconnectionCounter>();
     }
 }

1dfdad4315663f8de0a0493d98a9adae8a887c29
Ryu Shinmura <rysestock@gmail.com>
2026-02-03 03:51:56 +0900

    ビルドスクリプトを修正

--- tools/build-and-testinstall-mcv_alpha.ps1
+++ tools/build-and-testinstall-mcv_alpha.ps1
@@ -1,28 +1,28 @@
 ﻿$workspaceRoot = "C:\Users\ryu\Downloads\mcv"
 
 Set-Location $workspaceRoot/apps/mcv/src-tauri
-cargo tauri build --debug --features alpha
+cargo tauri build --features alpha
 
 Set-Location $workspaceRoot/apps/exe-plugin-sample/src-tauri
-cargo tauri build --debug # --features alpha
+cargo tauri build # --features alpha
 
 Set-Location $workspaceRoot/crates/plugin-dummy
-cargo build --features alpha
+cargo build --release --features alpha
 
 Set-Location $workspaceRoot/crates/plugin-exe-manager
-cargo build # --features alpha
+cargo build --release # --features alpha
 
 New-Item -ItemType Directory -Path $env:LOCALAPPDATA\MultiCommentViewer -Force
-Copy-Item -Path $workspaceRoot/target/debug/MultiCommentViewer.exe -Destination $env:LOCALAPPDATA\MultiCommentViewer\
-Copy-Item -Path $workspaceRoot/target/debug/MultiCommentViewer.pdb -Destination $env:LOCALAPPDATA\MultiCommentViewer\
+Copy-Item -Path $workspaceRoot/target/release/MultiCommentViewer.exe -Destination $env:LOCALAPPDATA\MultiCommentViewer\
+Copy-Item -Path $workspaceRoot/target/release/MultiCommentViewer.pdb -Destination $env:LOCALAPPDATA\MultiCommentViewer\
 New-Item -ItemType Directory -Path $env:LOCALAPPDATA\MultiCommentViewer\plugins -Force
-Copy-Item -Path $workspaceRoot/target/debug/plugin_dummy.dll -Destination $env:LOCALAPPDATA\MultiCommentViewer\plugins\
-Copy-Item -Path $workspaceRoot/target/debug/plugin_dummy.pdb -Destination $env:LOCALAPPDATA\MultiCommentViewer\plugins\
-Copy-Item -Path $workspaceRoot/target/debug/plugin_exe_manager.dll -Destination $env:LOCALAPPDATA\MultiCommentViewer\plugins\
-Copy-Item -Path $workspaceRoot/target/debug/plugin_exe_manager.pdb -Destination $env:LOCALAPPDATA\MultiCommentViewer\plugins\
+Copy-Item -Path $workspaceRoot/target/release/plugin_dummy.dll -Destination $env:LOCALAPPDATA\MultiCommentViewer\plugins\
+Copy-Item -Path $workspaceRoot/target/release/plugin_dummy.pdb -Destination $env:LOCALAPPDATA\MultiCommentViewer\plugins\
+Copy-Item -Path $workspaceRoot/target/release/plugin_exe_manager.dll -Destination $env:LOCALAPPDATA\MultiCommentViewer\plugins\
+Copy-Item -Path $workspaceRoot/target/release/plugin_exe_manager.pdb -Destination $env:LOCALAPPDATA\MultiCommentViewer\plugins\
 New-Item -ItemType Directory -Path $env:LOCALAPPDATA\MultiCommentViewer\plugins\exe-plugin-sample -Force
-Copy-Item -Path $workspaceRoot/target/debug/exe-plugin-sample.exe -Destination $env:LOCALAPPDATA\MultiCommentViewer\plugins\exe-plugin-sample\
-Copy-Item -Path $workspaceRoot/target/debug/exe_plugin_sample.pdb -Destination $env:LOCALAPPDATA\MultiCommentViewer\plugins\exe-plugin-sample\
+Copy-Item -Path $workspaceRoot/target/release/exe-plugin-sample.exe -Destination $env:LOCALAPPDATA\MultiCommentViewer\plugins\exe-plugin-sample\
+Copy-Item -Path $workspaceRoot/target/release/exe_plugin_sample.pdb -Destination $env:LOCALAPPDATA\MultiCommentViewer\plugins\exe-plugin-sample\
 $manifest = @{
     path = "exe-plugin-sample.exe"
 }

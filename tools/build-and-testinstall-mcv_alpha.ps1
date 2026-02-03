05f81a4963cda14dbe3aa62ba904ec4b375e01f3
Ryu Shinmura <rysestock@gmail.com>
2026-02-03 18:07:54 +0900

    ビルドスクリプトを修正

--- tools/build-and-testinstall-mcv_alpha.ps1
+++ tools/build-and-testinstall-mcv_alpha.ps1
@@ -1,8 +1,14 @@
 ﻿$workspaceRoot = "C:\Users\ryu\Downloads\mcv"
 
+Set-Location $workspaceRoot/apps/mcv
+npm install
+npm run build
 Set-Location $workspaceRoot/apps/mcv/src-tauri
 cargo tauri build --features alpha
 
+Set-Location $workspaceRoot/apps/exe-plugin-sample
+npm install
+npm run build
 Set-Location $workspaceRoot/apps/exe-plugin-sample/src-tauri
 cargo tauri build # --features alpha
 
@@ -11,6 +17,8 @@ cargo build --release --features alpha
 
 Set-Location $workspaceRoot/crates/plugin-exe-manager
 cargo build --release # --features alpha
+
+
 
 New-Item -ItemType Directory -Path $env:LOCALAPPDATA\MultiCommentViewer -Force
 Copy-Item -Path $workspaceRoot/target/release/MultiCommentViewer.exe -Destination $env:LOCALAPPDATA\MultiCommentViewer\
@@ -27,3 +35,25 @@ $manifest = @{
     path = "exe-plugin-sample.exe"
 }
 $manifest | ConvertTo-Json | Out-File -FilePath "$env:LOCALAPPDATA\MultiCommentViewer\plugins\exe-plugin-sample\manifest.json" -Encoding UTF8
+
+# plugin-youtube-live
+Set-Location $workspaceRoot/crates/plugin-youtube-live
+cargo build --release --features alpha
+New-Item -ItemType Directory -Path $env:LOCALAPPDATA\MultiCommentViewer\plugins\plugin-youtube-live -Force
+Copy-Item -Path $workspaceRoot/target/release/plugin_youtube_live.dll -Destination $env:LOCALAPPDATA\MultiCommentViewer\plugins\plugin-youtube-live\
+Copy-Item -Path $workspaceRoot/target/release/plugin_youtube_live.pdb -Destination $env:LOCALAPPDATA\MultiCommentViewer\plugins\plugin-youtube-live\
+$manifest = @{
+    path = "plugin_youtube_live.dll"
+}
+$manifest | ConvertTo-Json | Out-File -FilePath "$env:LOCALAPPDATA\MultiCommentViewer\plugins\plugin-youtube-live\manifest.json" -Encoding UTF8
+
+# plugin-sample
+Set-Location $workspaceRoot/crates/plugin-sample
+cargo build --release --features alpha
+New-Item -ItemType Directory -Path $env:LOCALAPPDATA\MultiCommentViewer\plugins\plugin-sample -Force
+Copy-Item -Path $workspaceRoot/target/release/plugin_sample.dll -Destination $env:LOCALAPPDATA\MultiCommentViewer\plugins\plugin-sample\
+Copy-Item -Path $workspaceRoot/target/release/plugin_sample.pdb -Destination $env:LOCALAPPDATA\MultiCommentViewer\plugins\plugin-sample\
+$manifest = @{
+    path = "plugin_sample.dll"
+}
+$manifest | ConvertTo-Json | Out-File -FilePath "$env:LOCALAPPDATA\MultiCommentViewer\plugins\plugin-sample\manifest.json" -Encoding UTF8

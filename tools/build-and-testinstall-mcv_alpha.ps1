ad3764980bab2278227b60e60daf242e1ddd0a49
Ryu Shinmura <rysestock@gmail.com>
2026-02-16 15:38:22 +0900

    ビルドスクリプトを修正

--- tools/build-and-testinstall-mcv_alpha.ps1
+++ tools/build-and-testinstall-mcv_alpha.ps1
@@ -1,14 +1,11 @@
 ﻿$workspaceRoot = "C:\Users\ryu\Downloads\mcv"
 
 # mcv
-Set-Location $workspaceRoot/packages/my-dataview
-npm install
-npm run build
-Set-Location $workspaceRoot/apps/mcv
-npm install
-npm run build
-Set-Location $workspaceRoot/apps/mcv/src-tauri
-cargo tauri build --features alpha
+npm install --prefix "$workspaceRoot/packages/my-dataview"
+npm run build --prefix "$workspaceRoot/packages/my-dataview"
+npm install --prefix "$workspaceRoot/apps/mcv"
+npm run build --prefix "$workspaceRoot/apps/mcv"
+cargo build --release --features alpha --manifest-path "$workspaceRoot/apps/mcv/src-tauri/Cargo.toml"
 New-Item -ItemType Directory -Path $env:LOCALAPPDATA\MultiCommentViewer -Force
 Copy-Item -Path $workspaceRoot/target/release/MultiCommentViewer.exe -Destination $env:LOCALAPPDATA\MultiCommentViewer\
 Copy-Item -Path $workspaceRoot/target/release/MultiCommentViewer.pdb -Destination $env:LOCALAPPDATA\MultiCommentViewer\
@@ -17,8 +14,7 @@ Copy-Item -Path $workspaceRoot/target/release/MultiCommentViewer.pdb -Destinatio
 New-Item -ItemType Directory -Path $env:LOCALAPPDATA\MultiCommentViewer\plugins -Force
 
 # plugin-dummy
-Set-Location $workspaceRoot/crates/plugin-dummy
-cargo build --release --features alpha
+cargo build --release --features alpha --manifest-path "$workspaceRoot/crates/plugin-dummy/Cargo.toml"
 Copy-Item -Path $workspaceRoot/target/release/plugin_dummy.dll -Destination $env:LOCALAPPDATA\MultiCommentViewer\plugins\
 Copy-Item -Path $workspaceRoot/target/release/plugin_dummy.pdb -Destination $env:LOCALAPPDATA\MultiCommentViewer\plugins\
 
@@ -35,11 +31,9 @@ Copy-Item -Path $workspaceRoot/target/release/plugin_exe_manager_v3.dll -Destina
 Copy-Item -Path $workspaceRoot/target/release/plugin_exe_manager_v3.pdb -Destination $env:LOCALAPPDATA\MultiCommentViewer\plugins\
 
 # exe-plugin-sample
-Set-Location $workspaceRoot/apps/exe-plugin-sample
-npm install
-npm run build
-Set-Location $workspaceRoot/apps/exe-plugin-sample/src-tauri
-cargo tauri build # --features alpha
+npm install --prefix "$workspaceRoot/apps/exe-plugin-sample"
+npm run build --prefix "$workspaceRoot/apps/exe-plugin-sample"
+cargo build --release --manifest-path "$workspaceRoot/apps/exe-plugin-sample/src-tauri/Cargo.toml"
 New-Item -ItemType Directory -Path $env:LOCALAPPDATA\MultiCommentViewer\plugins\exe-plugin-sample -Force
 Copy-Item -Path $workspaceRoot/target/release/exe-plugin-sample.exe -Destination $env:LOCALAPPDATA\MultiCommentViewer\plugins\exe-plugin-sample\
 Copy-Item -Path $workspaceRoot/target/release/exe_plugin_sample.pdb -Destination $env:LOCALAPPDATA\MultiCommentViewer\plugins\exe-plugin-sample\
@@ -49,8 +43,7 @@ $manifest = @{
 $manifest | ConvertTo-Json | Out-File -FilePath "$env:LOCALAPPDATA\MultiCommentViewer\plugins\exe-plugin-sample\manifest.json" -Encoding UTF8
 
 # plugin-youtube-live
-Set-Location $workspaceRoot/crates/plugin-youtube-live
-cargo build --release --features alpha
+cargo build --release --features alpha --manifest-path "$workspaceRoot/crates/plugin-youtube-live/Cargo.toml"
 New-Item -ItemType Directory -Path $env:LOCALAPPDATA\MultiCommentViewer\plugins\plugin-youtube-live -Force
 Copy-Item -Path $workspaceRoot/target/release/plugin_youtube_live.dll -Destination $env:LOCALAPPDATA\MultiCommentViewer\plugins\plugin-youtube-live\
 Copy-Item -Path $workspaceRoot/target/release/plugin_youtube_live.pdb -Destination $env:LOCALAPPDATA\MultiCommentViewer\plugins\plugin-youtube-live\
@@ -60,8 +53,7 @@ $manifest = @{
 $manifest | ConvertTo-Json | Out-File -FilePath "$env:LOCALAPPDATA\MultiCommentViewer\plugins\plugin-youtube-live\manifest.json" -Encoding UTF8
 
 # plugin-chrome-cookie
-Set-Location $workspaceRoot/crates/plugin-chrome-cookie
-cargo build --release --features alpha
+cargo build --release --features alpha --manifest-path "$workspaceRoot/crates/plugin-chrome-cookie/Cargo.toml"
 New-Item -ItemType Directory -Path $env:LOCALAPPDATA\MultiCommentViewer\plugins\plugin-chrome-cookie -Force
 Copy-Item -Path $workspaceRoot/target/release/plugin_chrome_cookie.dll -Destination $env:LOCALAPPDATA\MultiCommentViewer\plugins\plugin-chrome-cookie\
 Copy-Item -Path $workspaceRoot/target/release/plugin_chrome_cookie.pdb -Destination $env:LOCALAPPDATA\MultiCommentViewer\plugins\plugin-chrome-cookie\
@@ -70,9 +62,18 @@ $manifest = @{
 }
 $manifest | ConvertTo-Json | Out-File -FilePath "$env:LOCALAPPDATA\MultiCommentViewer\plugins\plugin-chrome-cookie\manifest.json" -Encoding UTF8
 
+# plugin-bouyomi
+cargo build --release --features alpha --manifest-path "$workspaceRoot/crates/plugin-bouyomi/Cargo.toml"
+New-Item -ItemType Directory -Path $env:LOCALAPPDATA\MultiCommentViewer\plugins\plugin-bouyomi -Force
+Copy-Item -Path $workspaceRoot/target/release/plugin_bouyomi.dll -Destination $env:LOCALAPPDATA\MultiCommentViewer\plugins\plugin-bouyomi\
+Copy-Item -Path $workspaceRoot/target/release/plugin_bouyomi.pdb -Destination $env:LOCALAPPDATA\MultiCommentViewer\plugins\plugin-bouyomi\
+$manifest = @{
+    path = "plugin_bouyomi.dll"
+}
+$manifest | ConvertTo-Json | Out-File -FilePath "$env:LOCALAPPDATA\MultiCommentViewer\plugins\plugin-bouyomi\manifest.json" -Encoding UTF8
+
 # plugin-sample-v3
-Set-Location $workspaceRoot/crates/plugin-sample-v3
-cargo build --release --features alpha
+cargo build --release --features alpha --manifest-path "$workspaceRoot/crates/plugin-sample-v3/Cargo.toml"
 New-Item -ItemType Directory -Path $env:LOCALAPPDATA\MultiCommentViewer\plugins\plugin-sample-v3 -Force
 Copy-Item -Path $workspaceRoot/target/release/plugin_sample_v3.dll -Destination $env:LOCALAPPDATA\MultiCommentViewer\plugins\plugin-sample-v3\
 Copy-Item -Path $workspaceRoot/target/release/plugin_sample_v3.pdb -Destination $env:LOCALAPPDATA\MultiCommentViewer\plugins\plugin-sample-v3\
